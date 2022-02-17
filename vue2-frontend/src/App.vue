08eda0a0bbd173811de2736fb9ec814f52f28fca
istvanjuhasz <istvan.juhasz@telcotrend.hu>
2022-02-17 13:16:44 +0100

    put SpeedDial component in app if role is admin

--- vue2-frontend/src/App.vue
+++ vue2-frontend/src/App.vue
@@ -1,28 +1,37 @@
 <template>
   <v-app id="app">
-    <NavBar/>
-    <br>
+    <NavBar />
+    <br />
     <SnackBar />
-      <router-view class="container"></router-view>
+    <SpeedDial v-if="activeUser.role === 'admin'"/>
+    <router-view class="container"></router-view>
     <FooterComp />
   </v-app>
 </template>
 
 <script>
-import NavBar from '@/components/NavBar.vue';
-import SnackBar from './components/SnackBar.vue';
-import FooterComp from  './components/FooterComp.vue';
+import NavBar from "@/components/NavBar.vue";
+import SnackBar from "./components/SnackBar.vue";
+import FooterComp from "./components/FooterComp.vue";
+import SpeedDial from "./components/SpeedDial.vue";
 export default {
   name: "App",
 
   components: {
     NavBar,
     FooterComp,
-    SnackBar
+    SnackBar,
+    SpeedDial,
   },
-
-  data: () => ({}),
-  methods: {},
+  data: () => ({
+  }),
+  computed: {
+    activeUser() {
+      return this.$store.getters["auth/activeUser"];
+    },
+  },
+  methods: {
+  },
 };
 </script>
 <style>
---
+++ vue2-frontend/src/components/SpeedDial.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="d-flex align-end flex-column mr-12">
+    <v-speed-dial
+      v-model="fab"
+      top
+      left
+      direction="bottom"
+      :transition="transition"
+    >
+      <template v-slot:activator>
+        <v-btn v-model="fab" color="blue darken-2" dark fab>
+          <v-icon v-if="fab"> mdi-close </v-icon>
+          <v-icon v-else> mdi-account-circle </v-icon>
+        </v-btn>
+      </template>
+      <v-btn fab dark small color="green">
+        <v-icon>mdi-pencil</v-icon>
+      </v-btn>
+      <v-btn fab dark small color="indigo">
+        <v-icon>mdi-plus</v-icon>
+      </v-btn>
+      <v-btn fab dark small color="red">
+        <v-icon>mdi-delete</v-icon>
+      </v-btn>
+    </v-speed-dial>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    direction: "bottom",
+    fab: false,
+    fling: false,
+    hover: true,
+    tabs: null,
+    top: false,
+    right: false,
+    bottom: true,
+    left: true,
+    transition: "slide-y-reverse-transition",
+  }),
+};
+</script>
+
+<style>
+</style>
