e97d7342bab7d80af23dbd99ef81fe735e187992
istvanjuhasz <istvan.juhasz@telcotrend.hu>
2022-02-18 10:15:13 +0100

    using loading spinner in home page

--- vue2-frontend/src/views/HomePage.vue
+++ vue2-frontend/src/views/HomePage.vue
@@ -1,67 +1,73 @@
 <template>
   <div>
-    <v-card class="mx-auto" v-if="content">
-      <v-img
-        class="white--text align-end"
-        height="200px"
-        :src="titleImagePath"
-        alt=""
-      >
-        <v-card-title v-if="content">{{
-          content.Title.titleDescription
-        }}</v-card-title>
-      </v-img>
+    <div class="d-flex justify-center align-center" v-if="loader">
+      <LoadingSpinner />
+    </div>
+    <div v-if="!loader">
+      <v-card class="mx-auto">
+        <v-img
+          class="white--text align-end"
+          height="200px"
+          :src="titleImagePath"
+          alt=""
+        >
+          <v-card-title v-if="content">{{
+            content.Title.titleDescription
+          }}</v-card-title>
+        </v-img>
 
-      <v-card-subtitle class="pb-0"> Number 10 </v-card-subtitle>
+        <v-card-subtitle class="pb-0"> Number 10 </v-card-subtitle>
 
-      <v-card-text class="text--primary">
-        <v-row>
-          <v-col>
-            {{ content.Heading.headingDescription }}
-          </v-col>
-        </v-row>
-        <div>Whitehaven Beach</div>
+        <v-card-text class="text--primary">
+          <v-row>
+            <v-col>
+              {{ content.Heading.headingDescription }}
+            </v-col>
+          </v-row>
+          <div>Whitehaven Beach</div>
 
-        <div></div>
-      </v-card-text>
+          <div></div>
+        </v-card-text>
 
-      <v-card-actions> </v-card-actions>
-    </v-card>
+        <v-card-actions> </v-card-actions>
+      </v-card>
+      <v-card class="mx-auto mt-6" v-intersect="showMoreContent" v-if="content">
+        <v-img
+          v-if="content"
+          class="white--text align-end"
+          height="200px"
+          :src="headingImagePath"
+          alt=""
+        >
+        </v-img>
 
-    <div v-else> a szerver nem elérhető!</div>
+        <v-card-subtitle class="pb-0"> Number 10 </v-card-subtitle>
 
-    <v-card class="mx-auto mt-6" v-intersect="showMoreContent" v-if="content">
-      <v-img
-      v-if="content"
-        class="white--text align-end"
-        height="200px"
-        :src="headingImagePath"
-        alt=""
-      >
-      </v-img>
+        <v-card-text class="text--primary">
+          <v-row v-if="content">
+            <v-col>
+              {{ content.Heading.headingDescription }}
+            </v-col>
+          </v-row>
+          <div>Whitehaven Beach</div>
 
-      <v-card-subtitle class="pb-0"> Number 10 </v-card-subtitle>
+          <div></div>
+        </v-card-text>
 
-      <v-card-text class="text--primary">
-        <v-row v-if="content">
-          <v-col>
-            {{ content.Heading.headingDescription }}
-          </v-col>
-        </v-row>
-        <div>Whitehaven Beach</div>
-
-        <div></div>
-      </v-card-text>
-
-      <v-card-actions> </v-card-actions>
-    </v-card>
+        <v-card-actions> </v-card-actions>
+      </v-card>
+    </div>
   </div>
 </template>
 
 <script>
 import HomePageService from "../services/homePageService";
+import LoadingSpinner from "../components/LoadingSpinner.vue";
 export default {
   name: "HomePage",
+  components: {
+    LoadingSpinner,
+  },
   data() {
     return {
       isContent: false,
@@ -69,10 +75,12 @@ export default {
       titleImagePath: "",
       headingImagePath: "",
       loadNewContent: false,
+      loader: false,
     };
   },
   created() {
     this.getHomePageData();
+    this.loader = true;
   },
   methods: {
     showMoreContent(entries) {
@@ -83,6 +91,7 @@ export default {
       HomePageService.getHomePage().then(
         (response) => {
           this.content = response.data.HomePage[0];
+          this.loader = false;
           this.isContent = true;
           this.titleImagePath =
             "http://localhost:8081/" + this.content.Title.titleImagePath;
